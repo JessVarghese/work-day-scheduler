434cd12fe68241b8f1abb54db5ff95a786c5cf61
Jess Varghese <jessvarghese@gmail.com>
2021-10-30 16:33:33 -0400

    fixed and updated rows to change color

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,67 +1,54 @@
 
 //Display current day at the top of the page
-const currentDay = moment().format("dddd, MMMM Do YYYY");
+let currentDay = moment().format("dddd, MMMM Do YYYY");
 document.getElementById('currentDay').innerHTML= currentDay;
 
 //check for current time
-const currentHour = moment().format("H"); 
-console.log(currentHour);
 
 
-$(document).ready(function(){
-    $(".saveBtn").click(function(){
-        var text = $(this).siblings(".description").val();
-        var time = $(this).parent().attr("id");
+let rows = document.getElementsByClassName("row-2");
+
+
     
-        localStorage.setItem(time, text);
-        localStorage.getItem(time, text);
-  });
-});
+    function getCurrentHour() {
 
+        let currentHour = moment().format("H"); 
 
-const rows = document.getElementsByClassName("row-2");
+        $(".row-2").each(function(){
+            rowHour = parseInt($(this).attr("id")); 
 
+        if (currentHour > rowHour) {
+                $(this).addClass("past");
 
-Array.from(rows).forEach(row => {
-    let rowIdString = row.id,
-      rowHour;
-    if (rowIdString) {
-      rowHour = parseInt(rowIdString);
-
-    }
-        if (rowHour) {
-
-            if (currentHour > rowHour) {
-             $(".description").addClass("past");
-
-      
-            } else if (currentHour < rowHour) {
-                $(".description").addClass("future");
-        
-            } else if (currentHour === rowHour) {
-                $(".description").addClass("present");
-        
+            } else if (currentHour == rowHour){
+                $(this).removeClass("past");
+                $(this).addClass("present");
+            
+            } else {
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+                $(this).addClass("future");
             }
-        }
-    });
-
     
-    
-//    function saveData () {
-//        var saveButton = document.getElementsByClassName("saveBtn");
-//        saveButton.addEventListener("click", saveData);
-
-//        var textInput = document.querySelectorAll("textarea");
-//        textInput.addEventListener("click", saveData);
-       
-       
-//         localStorage.setItem("saveButton", "saveButton");
-//         localStorage.setItem("textInput", "textInput");
-
-//         localStorage.getItem("saveButton");
-//        localStorage.getItem("textInput");
-//    }
-
+        });
+        console.log(currentHour);
+        console.log(rowHour);
+        console.log(rowHour[1]);
+    };
+        getCurrentHour();
 
 
     
+var saveButton = document.getElementsByClassName('saveBtn');
+saveButton.onclick = saveData;
+        
+
+    function saveData(){
+    var input = document.getElementsByClassName("description")    ;
+    localStorage.setItem("button", input.value);
+};
+
+
+    // // //create an onclick function for the save button
+    // // select the description
+    // set the value and time of that task
